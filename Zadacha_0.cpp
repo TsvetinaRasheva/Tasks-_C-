59729702be8b5da64dc77ac026f64b5427215ce3
Tsvetina Rasheva <35625821+TsvetinaRasheva@users.noreply.github.com>
2020-10-28 13:57:56 +0200

    Add files via upload

---
+++ Zadacha_0.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{	
+	 int number;
+	   int factorialSum = 1;
+
+	   cout << "Insert factorial number : ";
+	   cin >> number;
+
+	   for (int i = 1; i <= number; i++)
+	   {
+		   factorialSum *= i;
+	   }
+
+	   cout << "Factorial is: " << factorialSum;
+	 
+	   return 0;
+}
