52e4b2bdb2b88fa7e89bdfab58c87e4ba2e5aa6f
Tsvetina Rasheva <35625821+TsvetinaRasheva@users.noreply.github.com>
2020-11-05 09:25:51 +0200

    Add last task Bonus_1

---
+++ Tasks_Praktikum_05.11/Bonus_1.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Insert number: ";
+	int n;
+	cin >> n;
+	
+	//Първи ред 
+	for (size_t start_cols = 0; start_cols < n; start_cols++)
+	{
+		cout << "*";
+	}
+
+	cout << endl;
+
+	//Горна част на диаманта
+	for (int row = 1; row <= n / 3; row++)
+	{
+		//Лява част
+		for (int col = 0; col < (n + 1) / 2; col++)
+		{
+
+			if (col == row)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+
+		//Дясна част
+		for (int col = ((n + 1) / 2) - 2; col > 0; col--)
+		{
+			if (col == row)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+
+		cout << endl;
+	}
+
+	//Средна част
+	for (size_t middle_row = 1; middle_row <= n; middle_row++)
+	{
+		if (middle_row == (n + 1) / 2)
+		{
+			cout << "*";
+		}
+		else
+		{
+			cout << " ";
+		}
+	}
+
+	cout << endl;
+
+	//Долна част на диаманта
+	for (int row = n/3; row >= 1; row--)
+	{
+		//Лява част
+		for (int col = 0; col < (n + 1) / 2; col++)
+		{
+			if (col == row)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+
+		//Дясна част
+		for (int col = ((n + 1) / 2)-2; col >= 1; col--)
+		{
+			if (col == row)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+
+		cout << endl;
+	}
+	
+	//Последен ред 
+	for (size_t start_cols = 0; start_cols < n; start_cols++)
+	{
+		cout << "*";
+	}
+
+	return 0;
+}
